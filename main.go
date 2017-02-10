a15436abeb4b9e1f78bea46e85be05e744a12ffe
phouse512 <philiphouse2015@u.northwestern.edu>
2017-02-10 09:37:25 -0800

    added some db queries

--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	//"encoding/json"
+	"database/sql"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 	"io/ioutil"
 	"os"
 )
 
+var (
+	id int
+)
+
 func main() {
+
+	db, err := sql.Open("postgres", "postgres://phil:3YToxjG2hj3wYoWKc84a2AYYEBiiABTa@piper.phizzle.space/piperdb")
+
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	test_query, err := db.Query("SELECT id from commit_log limit 10")
+	if err != nil {
+		fmt.Printf("query failed: %v\n", err)
+	}
+
+	defer test_query.Close()
+	for test_query.Next() {
+		err := test_query.Scan(&id)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+		}
+		//		fmt.Printf("id: %v", id)
+	}
 
 	file, err := ioutil.ReadFile("polls.json")
 	if err != nil {
