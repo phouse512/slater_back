bafe1936bb6f2609e1eee12848ccf80da2d1d9f6
phouse512 <philiphouse2015@u.northwestern.edu>
2017-02-27 20:16:44 -0800

    adding getUser

---
+++ lambda/getUser/handler.py
@@ -0,0 +1,25 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+
+    bucket = 'slater-storage'
+    key = 'data/user.json'
+    
+    try:
+        data = s3.get_object(Bucket=bucket, Key=key)
+        json_data = data['Body'].read()
+        
+        return_object = {
+            'statusCode': 200,
+            'headers': {},
+            'body': json_data
+        }
+        return return_object
+    
+    except Exception as e:
+        print(e)
+        raise e
+
