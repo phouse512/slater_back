bb806ad8419de99080751406a2006f2d1995bde2
phouse512 <philiphouse2015@u.northwestern.edu>
2017-02-28 20:43:42 -0800

    adding db connection

--- lambda/slaterLogin/handler.py
+++ lambda/slaterLogin/handler.py
@@ -1,5 +1,7 @@
 import json
+import os
 import psycopg2
+
 
 def lambda_handler(event, context):
     # TODO implement
@@ -9,8 +11,16 @@ def lambda_handler(event, context):
     #   id = 2;
     #   username = phouse512
     #}
-    print("specially deployed")
-    
+
+    connection = psycopg2.connect(database=os.environ['db'],
+                                  user=os.environ['user'],
+                                  password=os.environ['password'],
+                                  host=os.environ['host'],
+                                  port=os.environ['port'])
+
+    print(event)
+    print(connection)
+
     auth_object = {
         'auth_token': 'fictional-auth-token',
         'created_at': '2017-02-26 22:53:13.143996',
