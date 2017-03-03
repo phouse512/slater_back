29e5dadd6e8fbf4280f946bc493f68a8c35852c5
phouse512 <philiphouse2015@u.northwestern.edu>
2017-03-02 19:16:32 -0800

    adding dynamic leaderboard

--- lambda/getLeaderboard/handler.py
+++ lambda/getLeaderboard/handler.py
@@ -1,25 +1,36 @@
-import json
 import boto3
+import json
+import os
+import psycopg2
 
 s3 = boto3.client('s3')
 
+
 def lambda_handler(event, context):
-    
-    bucket = 'slater-storage'
-    key = 'data/leaderboard.json'
-    
-    try:
-        data = s3.get_object(Bucket=bucket, Key=key)
-        json_data = data['Body'].read()
-        
-        return_object = {
-            'statusCode': 200,
-            'headers': {},
-            'body': json_data
-        }
-        return return_object
-    
-    except Exception as e:
-        print(e)
-        raise e
- 
+
+    connection = psycopg2.connect(database=os.environ['db'],
+                                  user=os.environ['user'],
+                                  password=os.environ['password'],
+                                  host=os.environ['host'],
+                                  port=os.environ['port'])
+
+    cursor = connection.cursor()
+    leaderboard_query = "select users.id, users.username, banks.balance from users left join banks on " \
+                        "banks.entity_id=users.id where banks.type='user' order by banks.balance desc"
+
+    cursor.execute(leaderboard_query)
+    results = cursor.fetchall()
+
+    all_users = []
+    for leader in results:
+        all_users.append({
+            'id': leader[0],
+            'username': leader[1],
+            'balance': leader[2]
+        })
+
+    return {
+        'statusCode': 200,
+        'headers': {},
+        'body': json.dumps(all_users)
+    }
